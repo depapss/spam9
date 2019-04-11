3e62ad182f94c43e74161ba2629a0b76414a09dd
wolfachraf5 <47570819+wolfachraf5@users.noreply.github.com>
2019-04-11 01:05:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("554282679718379543")
 setInterval(function() {
 channel.send(`***spam***`);
-}, 4)
+}, 10)
 })
 
 client.login(process.env.BOT_TOKEN);
